e20b7806d0d66d0d22992b958ace560612e1fc8b
dearQQ <372580696@qq.com>
2019-02-19 17:12:42 +0800

    基础版本

---
+++ Client/Assets/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Network.NetClient.Instance.Init("127.0.0.1",8000);
+        Network.NetClient.Instance.Connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
