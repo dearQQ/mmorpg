6b54b08d75d409c300ed0ae7bf5d14d8f3e697b6
dearQQ <372580696@qq.com>
2019-02-20 14:11:08 +0800

    添加helloworld版本

---
+++ Src/Client/Assets/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SkillBridge.Message;
+
+public class HelloWorld : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Network.NetClient.Instance.Init("127.0.0.1",8000);
+        Network.NetClient.Instance.Connect();
+        NetMessage msg = new NetMessage();
+        msg.Request = new NetMessageRequest();
+        msg.Request.firstTestReq = new FirstTestRequest();
+        msg.Request.firstTestReq.Text = "Hello World";
+        Network.NetClient.Instance.SendMessage(msg);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Src/Server/GameServer/GameServer/GameServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+
+using System.Threading;
+
+using Network;
+using GameServer.Services;
+
+namespace GameServer
+{
+    class GameServer
+    {
+        Thread thread;
+        bool running = false;
+        NetService netService;
+        public bool Init()
+        {
+            DBService.Instance.Init();
+            HelloWorldService.Instance.Init();
+            netService = new NetService();
+            netService.Init(8000);
+            thread = new Thread(new ThreadStart(this.Update));
+            return true;
+        }
+
+        public void Start()
+        {
+            netService.Start();
+            HelloWorldService.Instance.Start();
+            running = true;
+            thread.Start();
+        }
+
+
+        public void Stop()
+        {
+            netService.Stop();
+            running = false;
+            thread.Join();
+        }
+
+        public void Update()
+        {
+            while (running)
+            {
+                Time.Tick();
+                Thread.Sleep(100);
+                //Console.WriteLine("{0} {1} {2} {3} {4}", Time.deltaTime, Time.frameCount, Time.ticks, Time.time, Time.realtimeSinceStartup);
+            }
+        }
+    }
+}
