05b9f61d9fa06b05a9f4794916e7e4f728262e3f
dearQQ <372580696@qq.com>
2019-02-21 18:50:35 +0800

    add UserService

---
+++ Src/Server/GameServer/GameServer/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Network;
+using SkillBridge.Message;
+using GameServer.Entities;
+using System.Linq;
+
+namespace GameServer.Services
+{
+    class UserService :Singleton<UserService>
+    {
+        public UserService()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<UserRegisterRequest>(this.OnRegister);
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<UserLoginRequest>(this.OnLogin);
+        }
+        public void Init()
+        {
+
+        }
+        void OnRegister(NetConnection<NetSession> sender, UserRegisterRequest request)
+        {
+            NetMessage message= new NetMessage();
+            message.Response = new NetMessageResponse();
+            message.Response.userRegister = new UserRegisterResponse();
+
+            TUser user = DBService.Instance.Entities.Users.Where(u => u.Username == request.User).FirstOrDefault();
+            if (user != null)
+            {
+                message.Response.userRegister.Result = Result.Failed;
+                message.Response.userRegister.Errormsg = "用户已存在.";
+            }
+            else
+            {
+                TPlayer player = DBService.Instance.Entities.Players.Add(new TPlayer());
+                DBService.Instance.Entities.Users.Add(new TUser() { Username = request.User, Password = request.Passward, Player = player });
+                DBService.Instance.Entities.SaveChanges();
+                message.Response.userRegister.Result = Result.Success;
+                message.Response.userRegister.Errormsg = "用户注册成功";
+            }
+
+            byte[] data = PackageHandler.PackMessage(message);
+            sender.SendData(data, 0, data.Length);
+        }
+        void OnLogin(NetConnection<NetSession> sender, UserLoginRequest request)
+        {
+            NetMessage message = new NetMessage();
+            message.Response = new NetMessageResponse();
+            message.Response.userLogin = new UserLoginResponse();
+
+            TUser user = DBService.Instance.Entities.Users.Where(u => u.Username == request.User).FirstOrDefault();
+            if (user != null)
+            {
+                message.Response.userLogin.Result = Result.Success;
+                message.Response.userLogin.Errormsg = "none";
+            }
+            else
+            {
+                message.Response.userLogin.Result = Result.Failed;
+                message.Response.userLogin.Errormsg = "用户不存在";
+            }
+
+            byte[] data = PackageHandler.PackMessage(message);
+            sender.SendData(data, 0, data.Length);
+        }
+        
+    }
+}
