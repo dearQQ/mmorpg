1ca3f7125195304b5d411ad7df30a322917e64b1
dearQQ <372580696@qq.com>
2019-02-21 18:47:44 +0800

    资源提交

---
+++ Src/Client/Assets/Scripts/UI/UILogin.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Services;
+using SkillBridge.Message;
+using System;
+
+public class UILogin : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle isRemember;
+    [SerializeField]
+    private InputField txtUser;
+    [SerializeField]
+    private InputField txtPassword;
+    [SerializeField]
+    private Button btnLogin;
+
+    private void Start()
+    {
+        bool bRemember = PlayerPrefs.GetInt("isRemember") == 1;
+
+        Debug.Log(PlayerPrefs.GetInt("isRemember"));
+        Debug.Log(bRemember);
+        isRemember.isOn = bRemember;
+        if (bRemember)
+        {
+            txtUser.text = PlayerPrefs.GetString("user","");
+            txtPassword.text = PlayerPrefs.GetString("password","");
+        }
+        UserServive.Instance.Connect();
+        UserServive.Instance.OnLogin = this.OnLogin;
+    }
+
+    public void OnClickLogin()
+    {
+        if (string.IsNullOrEmpty(txtUser.text))
+        {
+            MessageBox.Show("请输入账号");
+            return;
+        }
+        if (string.IsNullOrEmpty(txtPassword.text))
+        {
+            MessageBox.Show("请输入密码");
+            return;
+        }
+        
+        PlayerPrefs.SetInt("isRemember",isRemember.isOn ? 1 : 0);
+        UserServive.Instance.Login(txtUser.text,txtPassword.text);
+    }
+
+    public void OnLogin(Result res,string msg)
+    {
+        if (res == Result.Success)
+        {
+            MessageBox.Show("登录成功", "用户登录");
+            if (isRemember.isOn)
+            {
+                PlayerPrefs.SetString("user", txtUser.text);
+                PlayerPrefs.SetString("password", txtPassword.text);
+            }
+        }
+        else
+            MessageBox.Show(msg, "用户登录", MessageBoxType.Error);
+    }
+
+}
---
+++ Src/Client/Assets/Scripts/UI/UIRegister.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Services;
+using SkillBridge.Message;
+
+public class UIRegister : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject RegisterUI;
+    [SerializeField]
+    private Button btnRegister;
+
+    [SerializeField]
+    private InputField txtUser;
+    [SerializeField]
+    private InputField txtPassword;
+    [SerializeField]
+    private InputField txtPasswordAffirm;
+
+    [SerializeField]
+    private InputField txtLoginUser;
+    [SerializeField]
+    private InputField txtLoginPassword;
+    [SerializeField]
+    private Toggle isRemember;
+
+    private void Start()
+    {
+        UserServive.Instance.OnRegister = this.OnRegister;
+    }
+    private void Awake()
+    {
+    }
+    private void OnEnable()
+    {
+        txtUser.text = "";
+        txtPassword.text = "";
+        txtPasswordAffirm.text = "";
+    }
+    public void OnClickRegister()
+    {
+        if (string.IsNullOrEmpty(txtUser.text))
+        {
+            MessageBox.Show("请输入用户名","用户注册");
+            return;
+        }
+        if (string.IsNullOrEmpty(txtPassword.text))
+        {
+            MessageBox.Show("请输入密码", "用户注册");
+            return;
+        }
+        if (string.IsNullOrEmpty(txtPasswordAffirm.text))
+        {
+            MessageBox.Show("请再次输入密码", "用户注册");
+            return;
+        }
+        if (!string.Equals(txtPassword.text,txtPasswordAffirm.text))
+        {
+            MessageBox.Show("两次输入的密码不一致", "用户注册");
+            return;
+        }
+        UserServive.Instance.Register(txtUser.text, txtPassword.text);
+
+    }
+    void OnRegister(Result result, string message)
+    {
+        if (result == Result.Success)
+        {
+            MessageBox.Show("注册成功", "用户注册");
+            RegisterUI.SetActive(false);
+            txtLoginUser.text = txtUser.text;
+            txtLoginPassword.text = txtPassword.text;
+            isRemember.isOn = true;
+        }
+        else
+        {
+            MessageBox.Show("注册失败:"+ message, "用户注册",MessageBoxType.Error);
+        }
+    }
+
+
+
+
+
+}
